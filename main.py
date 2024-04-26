99cf03fdec01d8c183ba6036bb545d5af338f556
girieiemcet01 <33678713-girieiemcet01@users.noreply.replit.com>
2024-04-26 14:11:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+# print (len(input("What is your name? ")))
+
+
+a=input(" ")
+print(a)
+b=input(" ")
+print(b)
+
+print("a:",b)
+print("b:",a)
